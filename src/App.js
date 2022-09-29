5c0890c1ffef4ded98374233bb6f7c51396b5513
pyeong777 <sdp1123@naver.com>
2022-09-29 21:40:04 +0900

    ✨ add dummyList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor'
+import DiaryList from './DiaryList';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+dayjs.locale('ko');
+
+const nowDate = dayjs().format('YYYY-MM-DD ddd HH:mm:ss');
+
+const dummyList = [
+  {
+    id: 1,
+    author: "id1",
+    content: "hihihi",
+    emotion: 3,
+    created_date: nowDate
+  },
+  {
+    id: 2,
+    author: "id2",
+    content: "hihihi",
+    emotion: 5,
+    created_date: nowDate
+   },
+   {
+    id: 3,
+    author: "id3",
+    content: "hihihi",
+    emotion: 1,
+    created_date: nowDate
+   }
+]
 
 function App() {
  return (
     <div className='App'>
       <DiaryEditor />
+      <DiaryList diaryList={dummyList}/>
  </div>
  );
 }
