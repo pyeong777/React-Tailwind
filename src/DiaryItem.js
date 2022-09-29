6f8907cbbdb40f010a5db1cfe791aae2b7256d79
pyeong777 <sdp1123@naver.com>
2022-09-29 23:06:00 +0900

    ✨ remove components

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,4 +1,11 @@
 const DiaryItem = ({onRemove, author, content, nowDate, emotion, id}) => {
+
+    const handleRemove = () => {
+        if(window.confirm(`${id+1}번째 일기를 정말 삭제하시겠습니까?`)) {
+            onRemove(id);
+        }
+    };
+
     return (
         <div className="p-20 mb-10 bg-gray-100">        
             <div className="pb-10 mb-10 border-gray-500 border-b-1">
@@ -8,12 +15,7 @@ const DiaryItem = ({onRemove, author, content, nowDate, emotion, id}) => {
             <div className="font-bold">
                 <p>{content}</p>
                 <button className="p-5 mt-25 w-100 outline outline-1 rounded-5 hover:bg-yellow-300 hover:text-white" 
-                        onClick={() => {
-                        if(window.confirm(`${id+1}번째 일기를 정말 삭제하시겠습니까?`)) {
-                            onRemove(id);
-                        }
-                    }}>삭제하기
-                </button>
+                        onClick={handleRemove}>삭제하기</button> 
             </div>
         </div>
     )
