3494945f9a20cd6af89844b2ff314b50770ae6ca
pyeong777 <sdp1123@naver.com>
2022-10-02 23:25:11 +0900

    ✨ add json data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import DiaryEditor from './DiaryEditor'
 import DiaryList from './DiaryList';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRef } from 'react';
 dayjs.locale('ko');
 
@@ -12,8 +12,31 @@ const [data, setData] = useState([]);
 
 const dataId = useRef(0);
 
+const nowDate = dayjs().format('YYYY-MM-DD ddd HH:mm:ss');
+
+const getData = async() => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/comments');
+  const jsonData = await res.json();
+  
+  const initData = jsonData.slice(0,20).map((it) => {
+    return {
+      author: it.email,
+      content: it.body,
+      emotion: Math.floor(Math.random() * 5) + 1,
+      created_date: nowDate,
+      id: dataId.current++
+    }
+  })
+
+  setData(initData);
+};
+
+useEffect(() => {
+  getData();
+}, []); 
+
 const onCreate = (author, content, emotion) => {
-  const nowDate = dayjs().format('YYYY-MM-DD ddd HH:mm:ss');
+  
   const newItem = {
     author,
     content,
